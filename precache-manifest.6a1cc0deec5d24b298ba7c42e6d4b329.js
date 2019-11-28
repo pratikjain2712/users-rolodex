4cdb2a6a3eb8b95dad3a244bd6c81b80e07fc562
Pratik <p.jain@interproductec.com>
2019-11-27 23:40:49 -0600

    Updates

---
+++ precache-manifest.6a1cc0deec5d24b298ba7c42e6d4b329.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e1dd5386540cb1e3193e566c36b87c",
+    "url": "/users-rolodex/index.html"
+  },
+  {
+    "revision": "408912e129dbd96bcd52",
+    "url": "/users-rolodex/static/css/main.64b4fbde.chunk.css"
+  },
+  {
+    "revision": "baea527e8c4e8c7d3b91",
+    "url": "/users-rolodex/static/js/2.8a85177f.chunk.js"
+  },
+  {
+    "revision": "408912e129dbd96bcd52",
+    "url": "/users-rolodex/static/js/main.a94f702f.chunk.js"
+  },
+  {
+    "revision": "721bacfe2d869301eca5",
+    "url": "/users-rolodex/static/js/runtime-main.f0bc2fd9.js"
+  }
+]);
